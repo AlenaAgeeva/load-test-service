7fad7c4ac58356a2b3cafa1ab267cba0b1108558
Alena Ageeva <alenaageevapv@gmail.com>
2026-03-23 19:28:54 -0300

    feat: add main application class for starting the application.

---
+++ src/main/java/ru/ageeva/loadtestservice/LoadTestServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.ageeva.loadtestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadTestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoadTestServiceApplication.class, args);
+    }
+
+}
